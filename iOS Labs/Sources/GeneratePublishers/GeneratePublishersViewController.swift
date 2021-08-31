aba7684c48e7197c41bef7c41df97158594ea094
Andrey Isaev <isaevav4@sovcombank.ru>
2021-08-31 14:32:02 +0500

    Added main Combine code

--- iOS Labs/Sources/GeneratePublishers/GeneratePublishersViewController.swift
+++ iOS Labs/Sources/GeneratePublishers/GeneratePublishersViewController.swift
@@ -6,68 +6,130 @@
 //
 
 import UIKit
-import Combine
+//import Combine
 
 final class GeneratePublishersViewController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var tableView: UITableView!
-    
-    private lazy var resultsController = UIViewController()
-    private lazy var searchController: UISearchController = {
-        let controller = UISearchController(searchResultsController: nil)
-        controller.automaticallyShowsCancelButton = false
-        controller.hidesNavigationBarDuringPresentation = false
-        controller.obscuresBackgroundDuringPresentation = true
-        controller.searchBar.delegate = self
-        return controller
-    }()
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     private enum State {
+        case just
         case future
         case passthoughSubject
         case published
-        case fromTheBox
     }
     
-    private var state: State = .future
+    private var state: State = .just
     
-    private var urlStrings = Array<String>.init(repeating: "URL Фотографии", count: 100)
+//    private let passthroughSubject = PassthroughSubject<String, Error>()
+//    private let input = Input()
+//    private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.searchController = searchController
+        button.layer.cornerRadius = 8.0
+        
+//        passthroughSubject
+//            .subscribe(on: RunLoop.main)
+//            .sink(
+//                receiveCompletion: { completion in
+//                    switch completion {
+//                    case .finished:
+//                        break
+//                    case .failure(let error):
+//                        print(error)
+//                    }
+//                },
+//                receiveValue: { [weak self] value in
+//                    self?.textLabel.text = value
+//                }
+//            ).store(in: &cancellables)
+//
+//        input.$text
+//            .subscribe(on: RunLoop.main)
+//            .sink(
+//                receiveCompletion: { completion in
+//                    switch completion {
+//                    case .finished:
+//                        break
+//                    case .failure(let error):
+//                        print(error)
+//                    }
+//                },
+//                receiveValue: { [weak self] value in
+//                    self?.textLabel.text = value
+//                }
+//            ).store(in: &cancellables)
     }
     
     @IBAction func handleSelected(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
-        case 0: state = .future
-        case 1: state = .passthoughSubject
-        case 2: state = .published
-        case 3: state = .fromTheBox
+        case 0:
+            state = .just
+        case 1:
+            state = .future
+        case 2:
+            state = .passthoughSubject
+        case 3:
+            state = .published
         default:
             fatalError("Unknown Index")
         }
     }
-}
 
-extension GeneratePublishersViewController: UISearchBarDelegate {
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let text = searchBar.text, !text.isEmpty else { return }
-        print("SEARCH TEXT")
-    }
-}
+    @IBAction func handleTapped(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        let timeString = formatter.string(from: Date())
+        let text = "Сообщение в " + timeString
+        
+        switch state {
+        case .just:
+//            Just(text)
+//                .sink(
+//                    receiveCompletion: { completion in
+//                        switch completion {
+//                        case .finished:
+//                            break
+//                        case .failure(let error):
+//                            print(error.localizedDescription)
+//                        }
+//                    },
+//                    receiveValue: { [weak self] value in
+//                        self?.textLabel.text = value
+//                    }
+//                ).store(in: &cancellables)
+            textLabel.text = text
 
-extension GeneratePublishersViewController: UITableViewDataSource {
+        case .future:
+//            Future<String, Error> { promise in
+//                promise(.success(text))
+//            }
+//            .sink(
+//                receiveCompletion: { completion in
+//                    switch completion {
+//                    case .finished:
+//                        break
+//                    case .failure(let error):
+//                        print(error)
+//                    }
+//                },
+//                receiveValue: { [weak self] value in
+//                    self?.textLabel.text = value
+//                }
+//            ).store(in: &cancellables)
+            textLabel.text = text
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        urlStrings.count
-    }
+        case .passthoughSubject:
+//            passthroughSubject.send(text)
+            textLabel.text = text
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "GeneratePublishersCell", for: indexPath)
-        cell.textLabel?.text = urlStrings[indexPath.row]
-        return cell
+        case .published:
+//            input.text = text
+            textLabel.text = text
+
+        }
     }
 }
---
+++ iOS Labs/Sources/GeneratePublishers/Input.swift
@@ -0,0 +1,15 @@
+//
+//  Input.swift
+//  iOS Labs
+//
+//  Created by Андрей Исаев on 31.08.2021.
+//
+
+import Foundation
+
+final class Input {
+    
+    @Published
+    var text: String = ""
+    
+}
